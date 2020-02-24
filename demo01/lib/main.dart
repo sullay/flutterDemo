6c26a3b3b2d37854099552d0c2a345bdb7040136
sullay <1181518458@qq.com>
2020-02-24 10:10:52 +0800

    test

--- demo01/lib/main.dart
+++ demo01/lib/main.dart
@@ -1,31 +1,25 @@
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
+void main() {
+  runApp(new MaterialApp(
+    title: 'Flutter Tutorial',
+    home: new TutorialHome(),
+  ));
+}
 
-  // Widget子类中的字段往往都会定义为"final"
-
-  final Widget title;
-
+class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      height: 56.0, // 单位是逻辑上的像素（并非真实的像素，类似于浏览器中的像素）
-      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-      decoration: new BoxDecoration(color: Colors.blue[500]),
-      // Row 是水平方向的线性布局（linear layout）
-      child: new Row(
-        //列表项的类型是 <Widget>
-        children: <Widget>[
-          new IconButton(
-            icon: new Icon(Icons.menu),
-            tooltip: 'Navigation menu',
-            onPressed: null, // null 会禁用 button
-          ),
-          // Expanded expands its child to fill the available space.
-          new Expanded(
-            child: title,
-          ),
+    //Scaffold是Material中主要的布局组件.
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new IconButton(
+          icon: new Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: new Text('Example title'),
+        actions: <Widget>[
           new IconButton(
             icon: new Icon(Icons.search),
             tooltip: 'Search',
@@ -33,40 +27,15 @@ class MyAppBar extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class MyScaffold extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // Material 是UI呈现的“一张纸”
-    return new Material(
-      // Column is 垂直方向的线性布局.
-      child: new SafeArea(
-        child: new Column(
-          children: <Widget>[
-            new MyAppBar(
-              title: new Text(
-                'Example title',
-                style: Theme.of(context).primaryTextTheme.title,
-              ),
-            ),
-            new Expanded(
-              child: new Center(
-                child: new Text('Hello, world!'),
-              ),
-            ),
-          ],
-        ),
+      //body占屏幕的大部分
+      body: new Center(
+        child: new Text('Hello, world!'),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: new Icon(Icons.add),
+        onPressed: null,
       ),
     );
   }
-}
-
-void main() {
-  runApp(new MaterialApp(
-    title: 'My app', // used by the OS task switcher
-    home: new MyScaffold(),
-  ));
 }
